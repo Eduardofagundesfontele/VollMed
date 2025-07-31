a1ebddd3243014b4be876d941e4db799afe88dec
Eduardofagundesfontele <eduardofagundesfontele3@gmail.com>
2025-07-31 08:52:57 -0300

    first commit

---
+++ src/main/java/med/voll/api/controller/Especialidade.java
@@ -0,0 +1,9 @@
+package med.voll.api.controller;
+
+public enum Especialidade {
+
+    ORTODEIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+}
---
+++ src/main/java/med/voll/api/medico/MedicoController.java
@@ -0,0 +1,24 @@
+package med.voll.api.medico;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+
+    }
+}
