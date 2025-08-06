072ef5a22a14affa1dd4b67d0a76df3ec138e1df
Eduardofagundesfontele <eduardofagundesfontele3@gmail.com>
2025-08-06 12:01:23 -0300

    feat(doctor): add PUT and DELETE endpoints

--- src/main/java/med/voll/api/medico/DadosListagemMedicos.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedicos.java
@@ -2,9 +2,9 @@ package med.voll.api.medico;
 
 import med.voll.api.controller.Especialidade;
 
-public record DadosListagemMedicos(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedicos(Long id,String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedicos(Medico medico){
-        this(medico.getNome(),medico.getEmail(),medico.getCrm(),medico.getEspecialidade());
+        this(medico.getId(),medico.getNome(),medico.getEmail(),medico.getCrm(),medico.getEspecialidade());
     }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -30,7 +31,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados){
+         this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -40,4 +44,20 @@ public class Medico {
 
     }
 
+    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
